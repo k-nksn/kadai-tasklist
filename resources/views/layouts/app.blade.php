acceeb6b770758d6c256d8b7e62e96eb33b458de
k-nksn <k.nksn0540@gmail.com>
2018-10-29 01:57:26 +0000

    bootstrap design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,12 +2,26 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
         <title>MessageBoard</title>
+        
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
     </head>
 
     <body>
-        @include('commons.error_messages')
-        
-        @yield('content')
+        @include('commons.navbar')
+        <div class="row">
+            <div class="col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-12">
+            @include('commons.error_messages')
+            
+            @yield('content')
+            </div>
+        </div>
     </body>
 </html>
--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -3,16 +3,25 @@
 @section('content')
 
 <h1>メッセージ新規作成ページ</h1>
+
+<div class="row">
+<div class="col-xs-6">
 {!! Form::model($message, ['route' => 'messages.store']) !!}
 
+    <div class="form-group">
     {!! Form::label('status', 'ステータス：') !!}
-    {!! Form::text('status') !!}
+    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+    </div>
     
+    <div class="form-group">
     {!! Form::label('content', 'メッセージ：') !!}
-    {!! Form::text('content') !!}
+    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+    </div>
     
-    {!! Form::submit('投稿') !!}
+    {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
 {!! Form::close() !!}
+</div>
+</div>
 
 @endsection
