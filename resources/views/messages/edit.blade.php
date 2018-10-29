c5032deb2e83afdc00645cf8c9d62860bf0cefc9
k-nksn <k.nksn0540@gmail.com>
2018-10-29 07:27:40 +0000

    modified edit.blade.php

--- resources/views/messages/edit.blade.php
+++ resources/views/messages/edit.blade.php
@@ -5,14 +5,15 @@
 <h1>id: {{ $message->id }} のメッセージ編集ページ</h1>
 
 {!! Form::model($message, ['route' => ['messages.update', $message->id], 'method' => 'put' ]) !!}
-
+<div class="form-group">
     {!! Form::label('status', 'ステータス：') !!}
-    {!! Form::text('status') !!}
-    
+    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
     {!! Form::label('content', 'メッセージ:') !!}
-    {!! Form::text('content') !!}
-
-    {!! Form::submit('更新') !!}
+    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+</div>
+    {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
 
 {!! Form::close() !!}
 
